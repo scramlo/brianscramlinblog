d3d88fa16142f4fdd69ec2758b8854e8b73e80e2
Brian Scramlin <bscramlin@campusedu.com>
2021-10-23 18:35:46 -0400

    Update head and use template parts for main nav

--- header.php
+++ header.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <title><?php wp_title(); ?></title>
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    <?php if (is_singular() && get_option('thread_comments')) wp_enqueue_script('comment-reply'); ?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title><?= bloginfo('title'); ?></title>
+    <?php wp_head(); ?>
 </head>
 
 <body>
-    <?php get_template_part('nav');
+    <?php get_template_part('template-parts/header/header-nav-main'); ?>
